cc408317e26a520873ce9917caf5226a2c1506e2
Jorge Zuluaga <zuluagajorge@gmail.com>
2014-06-14 23:37:04 -0500

    Update

--- index.php
+++ index.php
@@ -1,2 +1,22 @@
-<img src="images/udea.jpg" height=100>
-<img src="images/saa.jpg" height=100>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+</head>
+<center>
+<img src="images/aristarco.jpg" height=200>
+<H1>Ocultación de Marte por la Luna<br/>
+Julio 5 de 2014</H1>
+</center>
+
+<p>En esta página podrás calcular las condiciones de la ocultación de
+Marte por la Luna del próximo 5 de Julio de 2014 para tu ubicación
+específica.  Los observadores que se encuentran ubicados sobre la
+franja de totalidad (ver mapa abajo) pueden determinar aquí los
+tiempos de los contactos teniendo en cuenta o no el efecto de adelanto
+del fenómeno debido a la velocidad finita de la luz.</p>
+
+<H3>Tiempos de Contacto</H3>
+
+<H3>Franja de Totalidad</H3>
+
+</html>
